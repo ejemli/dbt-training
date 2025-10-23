d8a7d140081b7ef16bbab006f592d72893f83cd3
Emna Jemli <emna.jemli@talan.com>
2025-10-23 15:17:08 +0000

    developed raw_customer, raw_orders, raw_product

---
+++ models/raw/raw_customer.sql
@@ -0,0 +1,8 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select *
+from TALAN_ODS.DBT_EJEMLI_RAW_GLOBALMART.CUSTOMER
---
+++ models/raw/raw_orders.sql
@@ -0,0 +1,9 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+
+select * 
+from TALAN_ODS.DBT_EJEMLI_RAW_GLOBALMART.ORDERS
